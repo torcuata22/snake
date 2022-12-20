2d1e8df8beaa813f770881e3bf3b81cbac9b41fa
Marilyn Marquez <marilynmarquez@MMarquez.local>
2022-12-19 19:16:23 -0600

    solid snake, not moving

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("The Snake Game")
+#to get rid of flickering (delay in animation):
+screen.tracer(0) #turns tracer off, but you get black screen and nothing else
+
 
 #Step1: Create snake body (three squares, one next to the other)
 #turtle = 20X20 px, I can create with for loop, using tuples for positions:
@@ -12,9 +15,12 @@ segments = [] #start as empty list and append segments as they are created below
 for position in starting_positions:
     new_segment = Turtle("square")
     new_segment.color("white")
+    new_segment.penup()
     new_segment.goto(position)
     segments.append(new_segment)
 
+#to get fird of blank screen after tracer off:
+screen.update() #now snake shows as one piece, no delays
 game_is_on = True
 while game_is_on:
     for seg in segments:
