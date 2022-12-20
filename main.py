19995c8e30ad143c44444d9f3bbe3c246625b945
Marilyn Marquez <marilynmarquez@MMarquez.local>
2022-12-19 20:03:39 -0600

    OOP

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from turtle import Screen, Turtle
+from snake import Snake
 import time
 
 screen = Screen()
@@ -8,37 +9,16 @@ screen.title("The Snake Game")
 #to get rid of flickering (delay in animation):
 screen.tracer(0) #turns tracer off, but you get black screen and nothing else
 
-
-#Step1: Create snake body (three squares, one next to the other)
-#Step2: get the three little turtles to act like one
-#Step 3: link turtles so they move like a snake. Move n segment to n-1 position until we get to the head
+snake = Snake()
 
 
-#turtle = 20X20 px, I can create with for loop, using tuples for positions:
-starting_positions = [(0,0), (-20,0), (-40, 0)]
-segments = [] #start as empty list and append segments as they are created below
-for position in starting_positions:
-    new_segment = Turtle("square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(position)
-    segments.append(new_segment)
-
-#to get fird of blank screen after tracer off:
-#screen.update() #now snake shows as one piece, no delays, but no movement
 game_is_on = True
 while game_is_on:
+    #screen updates every 0.1 seconds
     screen.update() 
     time.sleep(0.1) 
-    # this made snake move forward as one, but we need body to follow head, so instead:
-    #loop from last to first segment, start at length(segments)-1, end at 0, step=-1:
-    for seg_num in range(len(segments)-1 ,0 ,-1): 
-        snake_position = segments[seg_num-1].position()
-        segments[seg_num].goto(snake_position)
-        #new_x = segments[seg_num - 1].xcor
-        #new_y = segments[seg_num - 1].ycor this doesn't work, gives error
-    #need to move the first segment:
-    segments[0].forward(20)
+    snake.move()
+    
 
 
 
---
+++ snake.py
@@ -0,0 +1,33 @@
+from turtle import Turtle
+#turtle = 20X20 px, I can create with for loop, using tuples for positions:
+STARTING_POSITIONS= [(0,0), (-20,0), (-40, 0)]
+MOVE_DISTANCE = 20
+
+#Creates a three-segment snake when instantiated
+class Snake:
+
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+
+
+    def create_snake(self):
+        segments = [] #start as empty list and append segments as they are created below
+        for position in STARTING_POSITIONS:
+            new_segment = Turtle("square")
+            new_segment.color("white")
+            new_segment.penup()
+            new_segment.goto(position)
+            self.segments.append(new_segment)
+
+    # snake needs to follow head, so loop from last to first segment 
+    # #start at length(segments)-1, end at 0, step=-1:
+    def move(self):
+        for seg_num in range(len(self.segments)-1 ,0 ,-1): 
+            snake_position = self.segments[seg_num-1].position()
+            self.segments[seg_num].goto(snake_position)
+            #new_x = segments[seg_num - 1].xcor
+            #new_y = segments[seg_num - 1].ycor this doesn't work, gives error
+        #need to move the first segment:
+        self.segments[0].forward(MOVE_DISTANCE)
+  
