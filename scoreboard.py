c75039d97fad093c0ca1014e1e95a57f6c183984
Marilyn Marquez <marilynmarquez@MMarquez.local>
2022-12-23 12:46:25 -0600

    high score

--- scoreboard.py
+++ scoreboard.py
@@ -9,6 +9,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -16,11 +17,19 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
        
     def update_scoreboard(self):
+        self.clear()
         self.write(f"Score = {self.score}", align = ALIGNMENT, font = FONT)  #font needs to be a tuple
 
-    def game_over(self):
-        self.goto(0,0)
-        self.write("GAME OVER", align = ALIGNMENT, font = FONT)
+    # def game_over(self):
+    #     self.goto(0,0)
+    #     self.write("GAME OVER", align = ALIGNMENT, font = FONT)
+
+    #to add the high score feature:
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_scoreboard()
 
     def add_to_score(self):
         self.score += 1
